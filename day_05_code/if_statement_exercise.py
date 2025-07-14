9792f22131188ce91b2652a59f63dbbeb2683834
Zahid Parviz <pervaizzahid55@gmail.com>
2025-07-14 17:19:56 +0500

    Daya 5 Code Practice Changes - Part 3

---
+++ day_05_code/if_statement_exercise.py
@@ -0,0 +1,62 @@
+# 5-3. Alien Colors -- 1
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("Color of alien is green")
+
+if alien_color == 'red':
+    print("Color of alient is red")
+
+
+# 5-4. Alien Colors -- 2
+alien_color = 'red'
+
+if alien_color == 'green':
+    print("Player just earned 5 points")
+else:
+    print("Player just earned 10 points")
+
+
+# 5-5. Alien Colors -- 3
+alien_color = 'yellow'
+if alien_color == 'green':
+    print("Player just earned 5 points")
+elif alien_color == 'yellow':
+    print("Player just earned 10 points")
+else:
+    print("Player just earned 15 points")
+
+
+# 5-6. Stages of Life
+age = 72
+
+if age < 2:
+    print("Person is a baby")
+elif age >= 2 and age < 4:
+        print("Person is a toddler")
+elif age >= 4 and age < 13:
+        print("Person is a kid")
+elif age >= 13 and age < 20:
+         print("Person is a teenager")
+elif age >= 20 and age < 65:
+         print("Person is an adult")
+elif age >= 65:
+          print("Person is an elder")
+
+# 5-7. Favorite Fruit
+favorite_fruits = ["apple", "banana", "orange"]
+
+if "orange" in favorite_fruits:
+      print("Orange is my favorite fruit")
+
+if "apple" in favorite_fruits:
+      print("Apple is my favorite fruit")
+
+if "pineapple" in favorite_fruits:
+      print("Pineapple is my favorite fruit")
+        
+if "grapes" in favorite_fruits:
+      print("Grape is my favorite fruit")
+
+if "banana" in favorite_fruits:
+      print("Banana is my favorite fruit")
