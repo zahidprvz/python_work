cda26e704b167169f2e29953ba24aed6d371d5d6
Zahid Pervaiz <pervaizzahid55@gmail.com>
2025-08-28 15:54:32 +0500

    Update classes_practice_p1.py

--- day_10_code/classes_practice_p1.py
+++ day_10_code/classes_practice_p1.py
@@ -1,86 +1,69 @@
-# 9-1 Restaurant
+# 9-1 Restaurant & Users Example
 
 class Restaurant:
-    def __init__(self, restaurant_name, cuisine_type):
-        self.restaurant_name = restaurant_name
-        self.cuisine_type = cuisine_type
+    def __init__(self, name, cuisine):
+        self.name = name
+        self.cuisine = cuisine
 
-    def describe_restaurant(self):
-        print(f"Restaurant Name is: {self.restaurant_name}")
-        print(f"Cuisine Type is: {self.cuisine_type}")
+    def describe(self):
+        print(f"Restaurant: {self.name}")
+        print(f"Cuisine: {self.cuisine}")
 
-    def open_restaurant(self):
-        print("Restaurant is Open!")
+    def open(self):
+        print(f"{self.name} is now open!")
 
-# Instantiation
 
+# Instantiate one restaurant
 my_restaurant = Restaurant("Baba Farid Hotel", "Desi")
 
-print(my_restaurant.restaurant_name)
-print(my_restaurant.cuisine_type)
+print(my_restaurant.name)
+print(my_restaurant.cuisine)
+print()
 
-print("\n")
-
-# Calling methods
-
-my_restaurant.open_restaurant()
-my_restaurant.describe_restaurant()
-
-print("\n")
+# Call methods
+my_restaurant.open()
+my_restaurant.describe()
+print()
 
 
-# 9-2 Three Restaurants
+# 9-2 Multiple Restaurants
+restaurants = [
+    Restaurant("Res1", "Cuisine1"),
+    Restaurant("Res2", "Cuisine2"),
+    Restaurant("Res3", "Cuisine3"),
+]
 
-restaurant_1 = Restaurant("res1", "cuis1")
-restaurant_2 = Restaurant("res2", "cuis2")
-restaurant_3 = Restaurant("res3", "cuis3")
+for r in restaurants:
+    r.describe()
+print()
 
-restaurant_1.describe_restaurant()
-restaurant_2.describe_restaurant()
-restaurant_3.describe_restaurant()
 
-print("\n")
-
-# 9-3 Users
-
+# 9-3 User Class
 class User:
     def __init__(self, first_name, last_name, email, password):
-        self.first_name = first_name
-        self.last_name = last_name
+        self.first_name = first_name.title()
+        self.last_name = last_name.title()
         self.email = email
         self.password = password
 
-    def describe_user(self):
-        print(f"Name is: {self.first_name + " " + self.last_name}")
-        print(f"Email is: {self.email}")
-        print(f"Password is: {self.password}")
+    def describe(self):
+        print(f"Name: {self.first_name} {self.last_name}")
+        print(f"Email: {self.email}")
+        print(f"Password: {self.password}")
 
-    def greet_user(self):
-        print(f"Hello, {self.first_name}, Your {self.email} is registered!")
+    def greet(self):
+        print(f"Hello {self.first_name}, your email {self.email} is registered!")
 
 
-user_1 = User("zahid", "parviz", "pervaizzahid55@gmail.com", "abc123")
-user_2 = User("namal", "hussain", "inamlfatime@gmail.com", "abc123")
-user_3 = User("zulfiqar", "khan", "zulfi@gmail.com", "cba321")
-user_4 = User("umair", "rajput", "umair@gmail.com", "zyr_abc")
+# Create users
+users = [
+    User("zahid", "parviz", "pervaizzahid55@gmail.com", "abc123"),
+    User("namal", "hussain", "inamlfatime@gmail.com", "abc123"),
+    User("zulfiqar", "khan", "zulfi@gmail.com", "cba321"),
+    User("umair", "rajput", "umair@gmail.com", "zyr_abc"),
+]
 
-user_1.describe_user()
-user_1.greet_user()
-
-print("\n")
-
-user_2.describe_user()
-user_2.greet_user()
-
-print("\n")
-
-user_3.describe_user()
-user_3.greet_user()
-
-print("\n")
-
-#
-#
-
-user_4.describe_user()
-user_4.greet_user()
+for u in users:
+    u.describe()
+    u.greet()
+    print()
